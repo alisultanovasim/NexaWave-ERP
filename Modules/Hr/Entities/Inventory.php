a1fdfc10ccb76c5004eb02058bf1773431f818ce
Zeka <zeka.abdinov@knexel.com>
2020-05-05 18:04:32 +0400

    inventory and uniform crud

---
+++ Modules/Hr/Entities/Inventory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Hr\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Inventory extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function inventoryType(){
+        return $this->belongsTo('Modules\Hr\Entities\InventoryType', 'inventory_type_id', 'id');
+    }
+
+    /**
+     * @param $query
+     * @param $companyId
+     * @return mixed
+     */
+    public function scopeCompanyId($query, $companyId){
+        return  $query->wherHas('inventoryType', function ($query) use ($companyId){
+            $query->where('company_id', $companyId);
+        });
+    }
+}
