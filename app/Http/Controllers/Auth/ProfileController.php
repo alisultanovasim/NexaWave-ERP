5ec0013ad55237c1ac73ed1fc2684d4617f7e92e
Вусал Шахбазов <vusa.013@gmail.com>
2020-05-29 10:49:22 +0400

    adding ambar logic

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -23,6 +23,45 @@ class ProfileController extends Controller
         $companies = null;
         $modules = null;
 
+        switch ($user->role_id) {
+
+            case User::OFFICE:
+                $office = OfficeUser::with(['office:id,name,image,company_id'])->where('user_id', $user->id)->first()->office;
+                $companies = [
+                    [
+                        "company_id" => $office->company_id
+                    ]
+                ];
+                break;
+
+            case User::EMPLOYEE:
+                $companies = Employee::with([
+                    'company',
+                    'contract' => function ($q) {
+                        $q->where('is_active', true);
+                    },
+                    'contract.position',
+                    'contract.position.permissions' => function ($q) {
+                        $q->with(['module:id,name'])
+                            ->whereHas('module', function ($q) {
+                                $q->whereNull('parent_id');
+                            })
+                            ->whereHas('permission' , function ($q) {
+                                $q->where('id', Permission::READ);
+                            })
+                            ->select([
+                                'position_id',
+                                'module_id',
+                                'permission_id',
+                            ]);
+                    },
+                ])
+                    ->active()
+                    ->where('user_id', Auth::id())
+                    ->get();
+                break;
+
+        }
 
 
         return $this->dataResponse([
@@ -31,6 +70,7 @@ class ProfileController extends Controller
             'modules' => $modules,
             'office' => $office
         ]);
+
     }
 
     public function index(Request $request)
