38ebe256b449b0aa9767f070757eb5feecfee252
Вусал Шахбазов <vusa.013@gmail.com>
2020-06-26 14:49:44 +0400

    delete dd on handler.php

--- Modules/Storage/Entities/ProductAssignment.php
+++ Modules/Storage/Entities/ProductAssignment.php
@@ -15,34 +15,44 @@ class ProductAssignment extends Model
     protected $guarded = [];
 
 
-    const ASSIGN_TO_USER = 1 ;
-    const ASSIGN_TO_PLACE = 2 ;
+    const ASSIGN_TO_USER = 1;
+    const ASSIGN_TO_PLACE = 2;
 
-    public function scopeCompany($q){
+    public function scopeCompany($q)
+    {
         return $q->where('company_id', request('company_id'));
     }
 
-    public function employee(){
+    public function employee()
+    {
         return $this->belongsTo(Employee::class);
     }
-    public function department(){
+
+    public function department()
+    {
         return $this->belongsTo(Department::class);
     }
-    public function section(){
+
+    public function section()
+    {
         return $this->belongsTo(Employee::class);
     }
-    public function sector(){
+
+    public function sector()
+    {
         return $this->belongsTo(Employee::class);
     }
-    public function product(){
+
+    public function product()
+    {
         return $this->belongsTo(Product::class);
     }
 
     public function setReasonsAttribute($value)
     {
         $this->attributes['reasons'] = \GuzzleHttp\json_encode($value);
-
     }
+
     public function getReasonsAttribute($value)
     {
         return \GuzzleHttp\json_decode($value);
