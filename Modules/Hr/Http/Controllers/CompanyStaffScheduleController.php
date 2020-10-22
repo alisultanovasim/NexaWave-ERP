dd757b0737a8fb6494053f3968cac71ee780a7ec
Zek <zekaabdinov91@gmail.com>
2020-10-22 15:15:24 +0400

    creating controller

---
+++ Modules/Hr/Http/Controllers/CompanyStaffScheduleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Hr\Http\Controllers;
+
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+class CompanyStaffScheduleController extends Controller
+{
+    use ApiResponse;
+
+    public function index(Request $request): JsonResponse
+    {
+        
+    }
+}
