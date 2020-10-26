546cbf0d40e1c6efc1a7aaadd86146c83d0294be
Ismat Babirli <i.babirli@outlook.com>
2020-10-26 10:32:22 +0400

    doc assignment issue fixed

--- Modules/Esd/Http/Controllers/AssignmentController.php
+++ Modules/Esd/Http/Controllers/AssignmentController.php
@@ -4,28 +4,38 @@
 namespace Modules\Esd\Http\Controllers;
 
 
-use App\Models\User;
+use App\Traits\ApiResponse;
+use App\Traits\DocumentBySection;
+use App\Traits\DocumentUploader;
 use Carbon\Carbon;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Modules\Esd\Entities\Assignment;
 use Modules\Esd\Entities\AssignmentItem;
 use Modules\Esd\Entities\Document;
 use Modules\Esd\Entities\Note;
-use App\Traits\ApiResponse;
-use App\Traits\DocumentBySection;
-use App\Traits\DocumentUploader;
-use Illuminate\Database\QueryException;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Routing\Controller;
 use Modules\Hr\Entities\Employee\Employee;
 
+/**
+ * Class AssignmentController
+ * @package Modules\Esd\Http\Controllers
+ */
 class AssignmentController extends Controller
 {
-    use  ApiResponse, DocumentUploader, DocumentBySection  ,ValidatesRequests;
+    use  ApiResponse, DocumentUploader, DocumentBySection, ValidatesRequests;
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function store(Request $request, $id)
     {
         $this->validate($request, [
@@ -36,6 +46,7 @@ class AssignmentController extends Controller
             "expire_time" => "sometimes|required|date|date_format:Y-m-d",
             'company_id' => 'required|integer',
         ]);
+
         if (!in_array($request->base, $request->user_ids))
             return $this->errorResponse(["base" => trans("apiResponse.notFound")]);
 
@@ -95,6 +106,12 @@ class AssignmentController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function update(Request $request, $id)
     {
         $this->validate($request, [
@@ -104,7 +121,7 @@ class AssignmentController extends Controller
             'user_ids' => 'sometimes|required|array',
             'user_ids.*' => 'sometimes|required|integer',
             'base' => 'required_with:user_ids|integer',
-            'is_extension' => ['required' , 'boolean']
+            'is_extension' => ['required', 'boolean']
         ]);
             DB::beginTransaction();
             $document = Document::where([
@@ -208,16 +225,22 @@ class AssignmentController extends Controller
             $document = Document::where("id", $id)
                 ->where("company_id", $company_id)
                 ->exists();
-            if (!$document)
-                return $this->errorResponse(trans("apiResponse.documentNotExists"));
-            Assignment::where("document_id", $id)
-                ->delete();
+        if (!$document)
+            return $this->errorResponse(trans("apiResponse.documentNotExists"));
+        Assignment::where("document_id", $id)
+            ->delete();
 
-            return $this->successResponse("OK");
+        return $this->successResponse("OK");
 
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function addUsersByMainAssignment(Request $request, $id)
     {
         $this->validate($request, [
@@ -273,6 +296,12 @@ class AssignmentController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function show(Request $request, $id)
     {
         $this->validate($request, [
@@ -280,17 +309,22 @@ class AssignmentController extends Controller
             'company_id' => 'required|integer',
         ]);
         $company_id = $request->company_id;
-            $document = Document::where("id", $id)
-                ->where("company_id", $company_id)
-                ->exists();
-            if (!$document)
-                return $this->errorResponse(trans("apiResponse.documentNotExists"));
-            $assignment = Assignment::with(['items', 'items.notes','items.rejects', 'items.employee.user'])
-                ->where('document_id', $id)->first();
-            return $this->successResponse($assignment);
+        $document = Document::where("id", $id)
+            ->where("company_id", $company_id)
+            ->exists();
+        if (!$document)
+            return $this->errorResponse(trans("apiResponse.documentNotExists"));
+        $assignment = Assignment::with(['items', 'items.notes', 'items.rejects', 'items.employee.user'])
+            ->where('document_id', $id)->first();
+        return $this->successResponse($assignment);
 
     }
 
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function index(Request $request)
     {
         $this->validate($request, [
@@ -311,7 +345,7 @@ class AssignmentController extends Controller
             $assignments->whereHas('items', function ($q) use ($request) {
                 if ($request->get('tome'))
                     $q->whereHas('employee' , function ($q){
-                        $q->where('user_id' , Auth::id());
+                        $q->where('user_id', Auth::id());
                     });
             });
         }
@@ -321,6 +355,12 @@ class AssignmentController extends Controller
         return $this->successResponse($assignments);
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function addNotes(Request $request, $id)
     {
         $this->validate($request, [
@@ -329,31 +369,40 @@ class AssignmentController extends Controller
             'company_id' => 'required|integer'
         ]);
         $company_id = $request->company_id;
-            $document = Document::where("id", $id)
-                ->where("company_id", $company_id)
-                ->where("status", Document::ACTIVE)
-                ->exists();
-            if (!$document)
-                return $this->errorResponse(trans("apiResponse.DocumentNotFound"));
+        $document = Document::where("id", $id)
+            ->where("company_id", $company_id)
+            ->where("status", Document::ACTIVE)
+            ->exists();
 
-            $assignment = Assignment::where("document_id", $id)
+
+        if (!$document)
+            return $this->errorResponse(trans("apiResponse.DocumentNotFound"));
+
+        $assignment = Assignment::where("document_id", $id)
+            ->first('id');
+        if (!$assignment)
+            return $this->errorResponse(trans("apiResponse.assignmentNotFound"));
+
+
+        $assignmentItem = AssignmentItem::where('assignment_id', $assignment->id)
+            ->where('user_id', Auth::user()->getEmployeeId($company_id))
                 ->first('id');
-            if (!$assignment)
-                return $this->errorResponse(trans("apiResponse.assignmentNotFound"));
 
+        if (!$assignmentItem)
+            return $this->errorResponse(trans('apiResponse.itemNotFound'));
 
-            $assignmentItem = AssignmentItem::where('assignment_id', $assignment->id)
-                ->where('user_id', Auth::id())
-                ->first('id');
-            if (!$assignmentItem)
-                return $this->errorResponse(trans('apiResponse.itemNotFound'));
-
-            $notes = array_merge($request->get('notes')??[] , $request->notes??[]);
-            Note::insert($this->saveNotes($assignmentItem, $notes, $request, $str = 'notes'));
-            return $this->successResponse("OK");
+        $notes = array_merge($request->get('notes') ?? [], $request->notes ?? []);
+        Note::insert($this->saveNotes($assignmentItem, $notes, $request, $str = 'notes'));
+        return $this->successResponse("OK");
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function updateNote(Request $request, $id)
     {
         $this->validate($request, [
@@ -363,7 +412,7 @@ class AssignmentController extends Controller
         ]);
         $company_id = $request->company_id;
 
-            $document = Document::where("id", $id)
+        $document = Document::where("id", $id)
                 ->where("company_id", $company_id)
                 ->where("status", Document::ACTIVE)
                 ->exists();
@@ -398,16 +447,22 @@ class AssignmentController extends Controller
             array_push($versions, $addingVersions);
 
 
-            $note->versions = json_encode($versions);
+        $note->versions = json_encode($versions);
 
-            $note->fill($this->saveNote($assignmentItem, $request->note, $request, 'notes'));
+        $note->fill($this->saveNote($assignmentItem, $request->note, $request, 'notes'));
 
-            $note->save();
+        $note->save();
 
-            return $this->successResponse("OK");
+        return $this->successResponse("OK");
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function deleteNote(Request $request, $id)
     {
         $this->validate($request, [
@@ -415,9 +470,9 @@ class AssignmentController extends Controller
 //            'user_id' => 'required|integer'
 
         ]);
-            $document = Document::where("id", $id)
-                ->where("company_id", $request->company_id)
-                ->where("status", Document::ACTIVE)
+        $document = Document::where("id", $id)
+            ->where("company_id", $request->company_id)
+            ->where("status", Document::ACTIVE)
                 ->exists();
             if (!$document)
                 return $this->errorResponse(trans("apiResponse.docStatusOrNotFound"));
@@ -439,14 +494,20 @@ class AssignmentController extends Controller
             $check = Note::where('assignment_item_id', $assignmentItem->id)
                 ->where('id', $request->note_id)
                 ->delete();
-            if (!$check)
-                return $this->errorResponse(trans("apiResponse.unProcess"));
+        if (!$check)
+            return $this->errorResponse(trans("apiResponse.unProcess"));
 
-            return $this->successResponse('OK');
+        return $this->successResponse('OK');
 
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function markAsRead(Request $request, $id)
     {
         $this->validate($request, [
@@ -454,9 +515,9 @@ class AssignmentController extends Controller
             'company_id' => 'required|integer'
         ]);
         $company_id = $request->company_id;
-            $assignment = Assignment::whereHas('document', function ($q) use ($id, $company_id) {
-                $q->where("id", $id)
-                    ->where("company_id", $company_id)
+        $assignment = Assignment::whereHas('document', function ($q) use ($id, $company_id) {
+            $q->where("id", $id)
+                ->where("company_id", $company_id)
                     ->where('status', Document::ACTIVE);
             })->where("document_id", $id)
                 ->first('id');
@@ -470,13 +531,19 @@ class AssignmentController extends Controller
                 ->update([
                     'status' => AssignmentItem::WAIT
                 ]);
-            if (!$check)
-                return $this->errorResponse(trans("apiResponse.unProcess"));
-            return $this->successResponse("OK");
+        if (!$check)
+            return $this->errorResponse(trans("apiResponse.unProcess"));
+        return $this->successResponse("OK");
 
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function done(Request $request, $id)
     {
         $this->validate($request, [
@@ -485,8 +552,8 @@ class AssignmentController extends Controller
             'return' => 'sometimes|required|boolean'
         ]);
         $company_id = $request->company_id;
-            $assignment = Assignment::whereHas('document', function ($q) use ($id, $company_id) {
-                $q->where("id", $id)
+        $assignment = Assignment::whereHas('document', function ($q) use ($id, $company_id) {
+            $q->where("id", $id)
                     ->where("company_id", $company_id)
                     ->where('status', Document::ACTIVE);
             })
@@ -513,15 +580,21 @@ class AssignmentController extends Controller
             if ($this->LastMakeDone($assignment->id , $assignmentItem->id)) Document::where('id', $id)->update(['status' => Document::WAIT_FOR_ACCEPTANCE]);
             else if ($assignmentItem->is_base and $this->issetSubAssigners($assignment->id)) return  $this->errorResponse(['error' => trans('apiResponse.subAssignedUserNotFinishWork')]);
 
-            $check = $assignmentItem->update(["status" => ($request->return) ? AssignmentItem::WAIT : AssignmentItem::DONE]);
+        $check = $assignmentItem->update(["status" => ($request->return) ? AssignmentItem::WAIT : AssignmentItem::DONE]);
 
-            if (!$check)
-                return $this->errorResponse(trans("apiResponse.unProcess"));
+        if (!$check)
+            return $this->errorResponse(trans("apiResponse.unProcess"));
 
-            return $this->successResponse("OK");
+        return $this->successResponse("OK");
 
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function changeStatus(Request $request, $id)
     {
         $this->validate($request, [
@@ -530,8 +603,8 @@ class AssignmentController extends Controller
             'return' => 'sometimes|required|boolean',
             'status' => 'required|integer|in:1,2,3'
         ]);
-            DB::beginTransaction();
-            $assignment = Assignment::whereHas('document', function ($q) use ($id, $request) {
+        DB::beginTransaction();
+        $assignment = Assignment::whereHas('document', function ($q) use ($id, $request) {
                 $q->where("id", $id)
                     ->where("company_id", $request->company_id)
                     ->where('status', Document::ACTIVE);
@@ -550,38 +623,54 @@ class AssignmentController extends Controller
                 if ($this->LastMakeDone($assignment->id , $assignmentItem->id)) Document::where('id', $id)->update(['status' => Document::WAIT_FOR_ACCEPTANCE]);
                 else if ($assignmentItem->is_base and $this->issetSubAssigners($assignment->id)) return  $this->errorResponse(['error' => trans('apiResponse.subAssignedUserNotFinishWork')]);
             }
-            AssignmentItem::where('id', $assignmentItem->id)
-                ->update([
-                    'status' => $request->status
-                ]);
-            DB::commit();
-            return $this->successResponse('OK');
+        AssignmentItem::where('id', $assignmentItem->id)
+            ->update([
+                'status' => $request->status
+            ]);
+        DB::commit();
+        return $this->successResponse('OK');
 
     }
 
-    protected function LastMakeDone($assignmentId , $without = null){
+    /**
+     * @param $assignmentId
+     * @param null $without
+     * @return bool
+     */
+    protected function LastMakeDone($assignmentId, $without = null)
+    {
         $obj = AssignmentItem::where('assignment_id', $assignmentId);
-            if($without != null)
-                $obj->where('id' , '!=' ,$without);
+        if ($without != null)
+            $obj->where('id', '!=', $without);
 
-        $bool = $obj->where('status', '!=', AssignmentItem::DONE) ->exists();
+        $bool = $obj->where('status', '!=', AssignmentItem::DONE)->exists();
 
         return !$bool;
     }
 
-    protected function issetSubAssigners($assignmentId){
-        return AssignmentItem::where('parent_id',$assignmentId)
+    /**
+     * @param $assignmentId
+     * @return mixed
+     */
+    protected function issetSubAssigners($assignmentId)
+    {
+        return AssignmentItem::where('parent_id', $assignmentId)
             ->where('status', "!=", AssignmentItem::DONE)
             ->exists();
     }
 
+    /**
+     * @param array $helper
+     * @param Request $request
+     * @return bool
+     */
     private function checkUser(array $helper, Request $request)
     {
-        $employees = Employee::whereIn('id' , $helper)
+        $employees = Employee::whereIn('id', $helper)
             ->where('company_id', $request->get('company_id'))
-            ->where('is_active' , true)
+            ->where('is_active', true)
             ->count();
-         return count($helper) == $employees;
+        return count($helper) == $employees;
     }
 
 }
