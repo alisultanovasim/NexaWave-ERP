39a7d663c70406c6f4a5d615e2384550e3e0b132
Ismat Babirli <i.babirli@outlook.com>
2020-10-27 12:39:57 +0400

    env changed to config in model for document,Contract,Location

--- Modules/Plaza/Entities/Document.php
+++ Modules/Plaza/Entities/Document.php
@@ -14,7 +14,7 @@ class Document extends Model
 
     public function getUrlAttribute($value){
         if ($value)
-            return env('APP_URL') . '/storage/' . $value;
+            return config('app.url') . '/storage/' . $value;
         return $value;
     }
 
