36c9a70d8481504ae08303ad450f7fd56a46a56a
Ismat Babirli <i.babirli@outlook.com>
2021-02-04 16:28:51 +0400

    floor issue fixed

--- Modules/Plaza/Entities/Floor.php
+++ Modules/Plaza/Entities/Floor.php
@@ -12,6 +12,7 @@ class Floor extends Model
 
     protected $guarded = ['id'];
 
+    protected $fillable=['office_id','number','common_size','solid_size'];
 
     public function offices(){
         return $this->belongsToMany('Modules\Plaza\Entities\Office' , 'offices_locations' , 'office_id' , 'floor_id');
