8712809142f9bad63cd8ddb0222851576b227e19
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-04-04 20:03:40 +0400

    test

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -9,9 +9,8 @@ use App\Providers\PermissionProvider;
 use App\Traits\ApiResponse;
 use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
-//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 
 class CheckUserRole
