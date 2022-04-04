7da8622cf137b1eb435cba8c6b1bbe08b2042a71
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-04-04 19:57:31 +0400

    test

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 
 use App\Models\Role;
-use App\Models\User;
 use App\Models\UserRole;
 use App\Providers\PermissionProvider;
 use App\Traits\ApiResponse;
@@ -12,9 +11,6 @@ use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Modules\Hr\Entities\Employee\Employee;
-use Modules\Plaza\Entities\OfficeUser;
 
 
 class CheckUserRole
@@ -27,6 +23,34 @@ class CheckUserRole
     private $request;
     private $roleModel;
 
+    private function apache_request_headers()
+    {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+
+        foreach ($_SERVER as $key => $val) {
+            if (preg_match($rx_http, $key)) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                // do some nasty string manipulations to restore the original letter case
+                // this should work in most cases
+                $rx_matches = explode('_', $arh_key);
+
+                if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+                    foreach ($rx_matches as $ak_key => $ak_val) {
+                        $rx_matches[$ak_key] = ucfirst($ak_val);
+                    }
+
+                    $arh_key = implode('-', $rx_matches);
+                }
+
+                $arh[$arh_key] = $val;
+            }
+        }
+
+        return ($arh);
+    }
+
     /**
      * CheckUserAccess constructor.
      * @param Request $request
@@ -35,12 +59,12 @@ class CheckUserRole
     public function __construct(Request $request, Role $role)
     {
         $this->request = $request;
-        if ($request->hasHeader('company_id')){
+        if ($request->hasHeader('company_id')) {
             $this->companyId = $request->header('company_id');
         } else {
-            $headers = apache_request_headers();
+            $headers = $this->apache_request_headers();
             dd($headers);
-            $this->companyId =  array_key_exists('company_id' , (array)$headers) ? $headers['company_id'] : $request->get('company_id');
+            $this->companyId = array_key_exists('company_id', (array)$headers) ? $headers['company_id'] : $request->get('company_id');
         }
         $this->userRoles = UserRole::where('user_id', Auth::id())->get(['role_id', 'company_id', 'office_id']);
         $this->permissionProvider = new PermissionProvider($role, $this->companyId);
@@ -53,15 +77,14 @@ class CheckUserRole
      * @return mixed
      * @throws AuthorizationException
      */
-    public function handle(Request $request, Closure $next){
+    public function handle(Request $request, Closure $next)
+    {
         /*
          * When user requests as company employee (sends company_id)
          */
-        if ($this->companyId){
+        if ($this->companyId) {
             $this->validateUserCompanyAndMergeToRequest($this->companyId);
-        }
-
-        /*
+        } /*
          * When user requests as platform user
          */
         else {
@@ -89,15 +112,16 @@ class CheckUserRole
     /**
      * @param $companyId
      */
-    private function validateUserCompanyAndMergeToRequest($companyId) {
+    private function validateUserCompanyAndMergeToRequest($companyId)
+    {
         $userRolesForThisRequest = [];
-        foreach ($this->userRoles as $role){
-            if ($role->company_id == $companyId){
+        foreach ($this->userRoles as $role) {
+            if ($role->company_id == $companyId) {
 
                 /*
                  * if user if office user set office id instance variable
                  */
-                if (Auth::user()->getAttribute('is_office_user')){
+                if (Auth::user()->getAttribute('is_office_user')) {
                     Auth::user()->setUserWorkingOfficeId($role['office_id']);
                 }
 
@@ -109,12 +133,13 @@ class CheckUserRole
     }
 
 
-    private function removeCompanyRolesFromUserRoleListForThisRequest(): void {
+    private function removeCompanyRolesFromUserRoleListForThisRequest(): void
+    {
         /*
          * Remove company roles
          */
-        foreach ($this->userRoles as $key => $role){
-            if ($role->company_id !== null){
+        foreach ($this->userRoles as $key => $role) {
+            if ($role->company_id !== null) {
                 unset($this->userRoles[$key]);
             }
         }
