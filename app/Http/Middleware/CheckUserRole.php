cdfe22b0f230c389e948db388f26ea10535a6679
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-04-04 20:00:08 +0400

    test

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -63,7 +63,8 @@ class CheckUserRole
             $this->companyId = $request->header('company_id');
         } else {
             $headers = apache_request_headers();
-            $this->companyId = array_key_exists('company_id', (array)$headers) ? $request->header('company_id') : $request->get('company_id');
+//            $this->companyId = array_key_exists('company_id', (array)$headers) ? $request->header('company_id') : $request->get('company_id');
+            $this->companyId = $request->header('company_id') !== null ? $request->header('company_id') : $request->get('company_id');
         }
         $this->userRoles = UserRole::where('user_id', Auth::id())->get(['role_id', 'company_id', 'office_id']);
         $this->permissionProvider = new PermissionProvider($role, $this->companyId);
