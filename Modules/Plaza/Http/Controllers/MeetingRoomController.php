24bd971b06d84ef9348ca019fcda2f1de953a1b0
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-04-06 16:28:44 +0400

    test

--- Modules/Plaza/Http/Controllers/MeetingRoomController.php
+++ Modules/Plaza/Http/Controllers/MeetingRoomController.php
@@ -128,7 +128,7 @@ class MeetingRoomController extends Controller
     public function uploadImage($company_id, $file, $str = 'rooms')
     {
         if ($file instanceof UploadedFile) {
-            return $file->store("/documents/$company_id/$str");
+            return $file->store("documents/$company_id/$str");
         }
 
         return null;
