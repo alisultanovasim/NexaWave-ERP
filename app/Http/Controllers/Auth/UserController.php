4f114bb5078e66e25f1374ac82b596e6aa4ccb1d
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-04-13 17:16:51 +0400

    test

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -587,7 +587,7 @@ class UserController extends Controller
         ]);
         if ($request->hasFile('avatar')) {
             $name = "$id.{$request->file('avatar')->getClientOriginalExtension()}";
-            $request->file('avatar')->move('documents/users/', $name);
+            $request->file('avatar')->move('documents/users/');
             $data['avatar'] = $name;
         }
         UserDetail::where('user_id', $id)
