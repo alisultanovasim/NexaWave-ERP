8d94f1c31adfca4562212a17b41c96e8aa791cd2
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-05-05 19:36:54 +0400

    test

--- Modules/Hr/Entities/Employee/Employee.php
+++ Modules/Hr/Entities/Employee/Employee.php
@@ -23,7 +23,7 @@ class Employee extends Model
 
     public function contracts()
     {
-        return $this->hasMany('Modules\Hr\Entities\Employee\Contract')->where('is_active' , 1)->where('is_terminated',0);
+        return $this->hasMany('Modules\Hr\Entities\Employee\Contract')->where('is_active' , 1);
     }
 
     public function contract()
