81c82e31cd440a1edb84ab892dffc034a18ff09e
user <bigitdedi@gmail.com>
2022-05-05 10:45:42 +0400

    Last Commit

--- Modules/Hr/Http/Controllers/Employee/EmployeeController.php
+++ Modules/Hr/Http/Controllers/Employee/EmployeeController.php
@@ -62,6 +62,9 @@ class EmployeeController extends Controller
             return $this->errorResponse($notExists);
 
         $employees = Employee::query()
+            ->whereHas('contract',function ($query){
+                $query->where('is_terminated','=',0);
+            })
             ->where('company_id', $request->get('company_id'))
             ->with("contracts");
         //            ->join('employee_contracts', 'employees.id', 'employee_contracts.employee_id');
