4d5f2f04a5e3752450e949d6699eb73ae9938d0b
user <bigitdedi@gmail.com>
2022-05-11 16:18:10 +0400

    Employee view

--- Modules/Hr/Http/Controllers/Employee/EmployeeController.php
+++ Modules/Hr/Http/Controllers/Employee/EmployeeController.php
@@ -63,10 +63,10 @@ class EmployeeController extends Controller
 
         $employees = Employee::query()
             ->where('company_id', $request->get('company_id'))
-            ->with("contracts")
-            ->whereHas('contract', function ($q) use ($request) {
-                $q->where('is_terminated', 0);
-            });
+            ->with("contracts");
+//            ->whereHas('contract', function ($q) use ($request) {
+//                $q->where('is_terminated', 0);
+//            });
         //            ->join('employee_contracts', 'employees.id', 'employee_contracts.employee_id');
 
         //        dd($employees->get()->toArray());
