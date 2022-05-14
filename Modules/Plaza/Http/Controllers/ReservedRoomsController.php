16140870f58d091e06352cbbd155f111301b9f40
user <bigitdedi@gmail.com>
2022-05-14 19:52:56 +0400

    Reserved Filters

--- Modules/Plaza/Http/Controllers/ReservedRoomsController.php
+++ Modules/Plaza/Http/Controllers/ReservedRoomsController.php
@@ -37,11 +37,11 @@ class ReservedRoomsController extends Controller
     public function filter(Request $request){
         $this->validate($request,[
             'company_id'=>'required',
-            'company_name'=>'sometimes|boolean',
-            'room_name'=>'sometimes|boolean',
-            'date'=>'sometimes|boolean',
+            'office_name'=>'sometimes|string',
+            'room_name'=>'sometimes|string',
+            'date'=>'sometimes|date',
             'status'=>'sometimes|in:0,1,2,3',
-            'price'=>'sometimes|boolean'
+            'price'=>'sometimes'
         ]);
 
 
@@ -50,47 +50,28 @@ class ReservedRoomsController extends Controller
         try {
             $data=\DB::table('meeting_room_reservations')
                 ->where('meeting_room_reservations.company_id',$request->company_id)
-                ->select('companies.name as company_name','meeting_rooms.name as room_name','start_at as date','meeting_room_reservations.status','price','meeting_rooms.name')
+                ->select('offices.name as office_name','companies.name as company_name','meeting_rooms.name as room_name','start_at as start_date','finish_at as end_date','meeting_room_reservations.status','price')
                 ->leftJoin('companies','companies.id','=','meeting_room_reservations.company_id')
-                ->leftJoin('meeting_rooms','meeting_rooms.id','=','meeting_room_reservations.meeting_room');
+                ->leftJoin('meeting_rooms','meeting_rooms.id','=','meeting_room_reservations.meeting_room')
+                ->leftJoin('offices','offices.company_id','=','meeting_room_reservations.company_id');
 
-            if ($request->has('company_name'))
-                if ($request->company_name==true){
-                    $data=$data->orderBy('companies.name','ASC');
-                }
-                else{
-                    $data=$data->orderBy('companies.name','DESC');
-                }
+            if ($request->has('office_name'))
+                $data->where('offices.name','like','%'.$request->get('office_name').'%');
 
 
 
             if ($request->has('room_name'))
-                if ($request->room_name==true){
-                    $data=$data->orderBy('meeting_rooms.name','ASC');
-                }
-                else{
-                    $data=$data->orderBy('meeting_rooms.name','DESC');
-                }
+                $data=$data->where('meeting_rooms.name','like','%'.$request->get('room_name').'%');
 
             if ($request->has('date'))
-                if ($request->date==true){
-                    $data=$data->orderBy('start_at','ASC');
-                }
-                else{
-                    $data=$data->orderBy('start_at','DESC');
-                }
+                $data=$data->where('meeting_room_reservations.start_at','like','%'.$request->get('date').'%');
 
 
             if ($request->has('status'))
                 $data=$data->where('meeting_room_reservations.status',$request->status);
 
             if ($request->has('price'))
-                if ($request->price==true){
-                    $data=$data->orderBy('price','ASC');
-                }
-                else{
-                    $data=$data->orderBy('price','DESC');
-                }
+                $data=$data->where('meeting_room_reservations.price','like','%'.$request->price.'%');
 
 
             return $this->dataResponse($data->get(),200);
