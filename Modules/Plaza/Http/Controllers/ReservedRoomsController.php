8066b30eae537b172501f4e1508d027efff308ed
kamrangaravaliyev <qaraveliyevkamran@gmail.com>
2022-05-15 23:23:41 +0400

    test

--- Modules/Plaza/Http/Controllers/ReservedRoomsController.php
+++ Modules/Plaza/Http/Controllers/ReservedRoomsController.php
@@ -28,7 +28,6 @@ class ReservedRoomsController extends Controller
             ->leftJoin('companies','companies.id','=','meeting_room_reservations.company_id')
             ->leftJoin('meeting_rooms','meeting_rooms.id','=','meeting_room_reservations.meeting_room')
             ->leftJoin('offices','offices.company_id','=','meeting_room_reservations.company_id')
-            ->groupBy('meeting_room_reservations.company_id')
             ->get();
         if ($reserved_rooms==null){
             return \response()->json(['message'=>'Rezerv edilmis otaq yoxdur'],404);
