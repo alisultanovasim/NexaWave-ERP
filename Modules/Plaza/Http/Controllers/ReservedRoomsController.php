37c63e405a3d84fcd15436f0e1392ecc7ef5ca91
user <bigitdedi@gmail.com>
2022-05-16 20:46:47 +0400

    hgyj

--- Modules/Plaza/Http/Controllers/ReservedRoomsController.php
+++ Modules/Plaza/Http/Controllers/ReservedRoomsController.php
@@ -25,9 +25,9 @@ class ReservedRoomsController extends Controller
             $reserved_rooms = \DB::table('meeting_room_reservations')
                 ->where(['meeting_room_reservations.office_id' => $request->office_id, 'meeting_room_reservations.company_id' => $request->company_id])
                 ->select('offices.name as office_name', 'companies.name as company_name', 'meeting_rooms.name as room_name', 'start_at as start_date', 'finish_at as end_date', 'meeting_room_reservations.status', 'price')
-                ->leftJoin('companies', 'companies.id', '=', 'meeting_room_reservations.company_id')
-                ->leftJoin('meeting_rooms', 'meeting_rooms.id', '=', 'meeting_room_reservations.meeting_room')
-                ->leftJoin('offices', 'offices.company_id', '=', 'meeting_room_reservations.company_id')
+                ->join('companies', 'companies.id', '=', 'meeting_room_reservations.company_id')
+                ->join('meeting_rooms', 'meeting_rooms.id', '=', 'meeting_room_reservations.meeting_room')
+                ->join('offices', 'offices.company_id', '=', 'meeting_room_reservations.company_id')
                 ->orderBy('meeting_room_reservations.start_at', 'desc')
                 ->paginate($per_page);
         } else {
