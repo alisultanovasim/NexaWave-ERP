7111fb2e2231de1461b92e728fc0aa66546e79b5
user <bigitdedi@gmail.com>
2022-05-16 12:05:18 +0400

    Commited by ASIM

--- Modules/Plaza/Http/Controllers/MeetingRoomController.php
+++ Modules/Plaza/Http/Controllers/MeetingRoomController.php
@@ -434,7 +434,7 @@ class MeetingRoomController extends Controller
         Meeting::create($request->only('company_id', 'start_at', 'finish_at', 'office_id', 'finish_at', 'event_name', 'description', 'meeting_room'));
         //Send email to plaza
 //        dispatch(new ReservationEmail("isa.qurbanov996@gmail.com", $office->name, $start, $meeting_rooms->name));
-        Mail::to("info@timetower.az")->send(new ReservationEmail("info@timetower.az", $office->name, $start, $meeting_rooms->name));
+//        Mail::to("info@timetower.az")->send(new ReservationEmail("info@timetower.az", $office->name, $start, $meeting_rooms->name));
         return $this->successResponse('OK');
     }
 
