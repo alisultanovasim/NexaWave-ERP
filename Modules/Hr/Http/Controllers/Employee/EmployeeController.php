d4f86b356baf92983c0007878ee14ae4aeeb7063
user <bigitdedi@gmail.com>
2022-05-17 15:44:51 +0400

    send password to email

--- Modules/Hr/Http/Controllers/Employee/EmployeeController.php
+++ Modules/Hr/Http/Controllers/Employee/EmployeeController.php
@@ -196,7 +196,7 @@ class EmployeeController extends Controller
             'passport_from_organ'=>'required|string',
 //            'date_of_issue'=>'required|date',
 //            'duration_type_id'=>'required|date',
-            'email'=>'required|email|unique:users,email'
+            'email'=>'sometimes|email|unique:users,email'
 
 
         ]);
