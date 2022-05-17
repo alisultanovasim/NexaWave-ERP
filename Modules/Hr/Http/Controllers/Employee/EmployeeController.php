40500d86f5d4aecfca0f65218d366cdbc4d382e7
user <bigitdedi@gmail.com>
2022-05-17 13:30:00 +0400

    send password to email

--- Modules/Hr/Http/Controllers/Employee/EmployeeController.php
+++ Modules/Hr/Http/Controllers/Employee/EmployeeController.php
@@ -180,10 +180,10 @@ class EmployeeController extends Controller
             'user_id' => ['sometimes', 'required', 'integer'],
             'create_contract' => ['required', 'boolean'],
             'tabel_no' => ['required', 'string', 'max:255'],
-            'roles' => ['required', 'array', 'min:1'],
-            'roles.*' => [
-                Rule::exists('roles', 'id')->where('company_id', $request->get('company_id'))
-            ],
+//            'roles' => ['required', 'array', 'min:1'],
+//            'roles.*' => [
+//                Rule::exists('roles', 'id')->where('company_id', $request->get('company_id'))
+//            ],
             'name'=>'required|min:3|max:77',
             'surname'=>'required|min:3|max:77',
             'father_name'=>'required|min:3|max:77',
