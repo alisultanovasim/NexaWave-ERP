3e631b2442f09f35ee62dfa5906a95e7fc26310c
user <bigitdedi@gmail.com>
2022-05-17 14:12:28 +0400

    send password to email

--- Modules/Hr/Http/Controllers/Employee/ContractController.php
+++ Modules/Hr/Http/Controllers/Employee/ContractController.php
@@ -268,7 +268,7 @@ class ContractController extends Controller
             $employee=Employee::query()
                 ->where('id',$request->employee_id)
                 ->first();
-            $pass=Str::random(7);
+            $pass=Str::random(6);
             $user=\App\Models\User::query()
                 ->where('id',$employee->user_id)
                 ->first();
