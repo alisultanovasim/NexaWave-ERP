770cdae95755a642045926fd63f109841aa665e9
user <bigitdedi@gmail.com>
2022-06-03 16:03:48 +0400

    Revert "Reference offices"
    This reverts commit dc1a44f9190d149cd43c4b80b408b8c0cb81d281.

--- app/Models/ReferenceOffice.php
+++ app/Models/ReferenceOffice.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReferenceOffice extends Model
 {
-    use SoftDeletes;
     protected $table='reference_offices';
     protected $fillable=['ref_name'];
 }
