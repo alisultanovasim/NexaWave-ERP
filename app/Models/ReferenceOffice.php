dc1a44f9190d149cd43c4b80b408b8c0cb81d281
user <bigitdedi@gmail.com>
2022-06-03 14:56:37 +0400

    Reference offices

--- app/Models/ReferenceOffice.php
+++ app/Models/ReferenceOffice.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReferenceOffice extends Model
 {
+    use SoftDeletes;
     protected $table='reference_offices';
     protected $fillable=['ref_name'];
 }
