d21ed3008e355be64b7e86f73c4f3ba586b8ec79
user <bigitdedi@gmail.com>
2022-06-27 14:57:31 +0400

    doesntHaveContracts

--- Modules/Hr/Http/Controllers/CompanyStructureController.php
+++ Modules/Hr/Http/Controllers/CompanyStructureController.php
@@ -105,7 +105,7 @@ class CompanyStructureController extends Controller
 
         $employees = Employee::with('user:id,name,surname')
             ->where(function ($q) use ($request) {
-                $q->doesntHave("contracts")
+                $q->whereHas("contracts")
                     ->orWhereHas('contracts', function ($query) use ($request) {
                         if ($request->get('department_id')) {
                             $query->where('department_id', $request->get('department_id'));
